1631d32310b483283b5d25acc2e1bedd8c1731ac
Cortland Starrett <cort@roxsoftware.com>
2023-03-24 19:38:47 -0400

    job:  #MUN-173  deal better with nested forks
    Pop the fork when the fork ends.  The propagated merge usage is now
    carried in the SequenceDefn.

--- tests/regress_w_compare.sh
+++ tests/regress_w_compare.sh
@@ -12,3 +12,5 @@ tc=t05
 python ../src/__main__.py t05_switch.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
 tc=t06
 python ../src/__main__.py t06_mixed.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t07
+python ../src/__main__.py t07_mixed2.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
