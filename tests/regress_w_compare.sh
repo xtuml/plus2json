b5d9d642a83a8e989253830a43cb06e30786f32a
Cortland Starrett <cort@roxsoftware.com>
2023-03-24 13:44:59 -0400

    job:  #MUN-173  adding tests with golden output
    This is the start of adding more tests with a comparison to correct output.
    These are using the -j -p option.

---
+++ tests/regress_w_compare.sh
@@ -0,0 +1,14 @@
+# Regression test plus2json
+
+tc=t01
+python ../src/__main__.py t01_straight.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t02
+python ../src/__main__.py t02_fork.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t03
+python ../src/__main__.py t03_split.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t04
+python ../src/__main__.py t04_if.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t05
+python ../src/__main__.py t05_switch.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
+tc=t06
+python ../src/__main__.py t06_mixed.puml -p -j > /tmp/o.txt && diff -q /tmp/o.txt $tc-j-p.txt && echo "$tc passed" || echo "$tc failed"
