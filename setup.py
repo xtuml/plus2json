5dadc60d8e04577c5c72f4e526686cf92905e649
Freddie Mather <freddie.mather@smartdcs.co.uk>
2023-03-31 10:03:08 +0000

    Fix setup.py .md -> .adoc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
+with open(path.join(this_directory, 'README.adoc')) as f:
     long_description = f.read()
 
 setup(name='plusact',
