414693cb8cd4b85922e00e9ea35b6aaab97018ed
Levi Starrett <levi@roxsoftware.com>
2023-06-13 14:31:29 -0400

    job MUN2-102: fixed up argument parser

--- plus2json/plus2json.py
+++ plus2json/plus2json.py
@@ -27,9 +27,39 @@ def main():
     # get version
     version = json.loads(files(__package__).joinpath('version.json').read_text())
 
-    # parse command line
-    parent_parser = argparse.ArgumentParser(add_help=False)
-    global_options = parent_parser.add_argument_group(title='Global Options')
+    # configure command line
+    parser = argparse.ArgumentParser(prog='plus2json',
+                                     formatter_class=argparse.RawTextHelpFormatter,
+                                     usage='python plus2json.pyz <command> [-v] [-h] [--debug] [--pretty-print] [-o dir]  [filenames ...]',
+                                     description='plus2json is a utility for processing PLUS job definitions and producing JSON ouptut for congifuring and testing the protocol verifier.',
+                                     epilog=textwrap.dedent('''\
+                                             Examples:
+                                                 # load Tutorial_1.puml and print a human readable version of the definition to the console
+                                                 python plus2json.pyz job Tutorial_1.puml --pretty-print
+
+                                                 # convert Tutorial_1.puml into JSON and output to the console
+                                                 python plus2json.pyz job Tutorial_1.puml
+
+                                                 # convert all .puml files in the 'puml' directory and write each to a JSON file in 'job_definitions'
+                                                 python plus2json.pyz job -o job_definitions puml/*.puml
+                                                 # load Tutorial_1.puml and print a human readable stream of events to the console
+                                                 python plus2json.pyz play Tutorial_1.puml --pretty-print
+
+                                                 # load Tutorial_1.puml and print a JSON stream of events to the console
+                                                 python plus2json.pyz play Tutorial_1.puml
+
+                                                 # load all .puml files in the 'puml' directory and write steam of events for each to a JSON file in 'job_definitions'
+                                                 python plus2json.pyz play -o job_definitions puml/*.puml
+                                     '''), add_help=False)
+
+    # commands
+    commands_group = parser.add_argument_group(title='Commands')
+    commands = commands_group.add_mutually_exclusive_group()
+    commands.add_argument('--job', action='store_true', help='Ouput PLUS job definition')
+    commands.add_argument('--play', action='store_true', help='Generate runtime event data')
+
+    # global options
+    global_options = parser.add_argument_group(title='Global Options')
     global_options.add_argument('-v', '--version', action='version', version=f'plus2json v{version["version"]} ({version["build_id"]})')
     global_options.add_argument('-h', '--help', action='help', help='Show this help message and exit')
     global_options.add_argument('--debug', action='store_true', help='Enable debug logging')
@@ -37,57 +67,13 @@ def main():
     global_options.add_argument('-o', '--outdir', metavar='dir', help='Path to output directory')
     global_options.add_argument('filenames', nargs='*', help='Input .puml files')
 
-    parser = argparse.ArgumentParser(prog='plus2json',
-                                     formatter_class=argparse.RawTextHelpFormatter,
-                                     usage='python plus2json.pyz <command> [-v] [-h] [--debug] [--pretty-print] [-o dir]  [filenames ...]',
-                                     description=textwrap.dedent('''\
-                                         plus2json is a utility for processing PLUS job definitions and producing
-                                         JSON ouptut for congifuring and testing the protocol verifier.'''),
-                                     epilog='See individual command help for usage examples.\n\n',
-                                     parents=[parent_parser], add_help=False)
-    subparsers = parser.add_subparsers(title='Commands', required=False)
-
-    # job command parser
-    job_parser = subparsers.add_parser('job', help='Ouput PLUS job definition', parents=[parent_parser], add_help=False,
-                                       formatter_class=argparse.RawTextHelpFormatter,
-                                       usage='python plus2json.pyz job [-v] [-h] [--debug] [--pretty-print] [-o dir] [filenames ...]',
-                                       description='Process the PLUS file(s) and output the resulting JSON job definition(s).',
-                                       epilog=textwrap.dedent('''\
-                                           Examples:
-                                               # load Tutorial_1.puml and print a human readable version of the definition to the console
-                                               python plus2json.pyz job Tutorial_1.puml --pretty-print
-
-                                               # convert Tutorial_1.puml into JSON and output to the console
-                                               python plus2json.pyz job Tutorial_1.puml
-
-                                               # convert all .puml files in the 'puml' directory and write each to a JSON file in 'job_definitions'
-                                               python plus2json.pyz job -o job_definitions puml/*.puml
-                                       '''))
-    job_parser.set_defaults(func=job)
-
-    # play command parser
-    play_parser = subparsers.add_parser('play', help='Generate runtime event data', parents=[parent_parser], add_help=False,
-                                        formatter_class=argparse.RawTextHelpFormatter,
-                                        # usage='python plus2json.pyz play [-v] [-h] [--debug] [--pretty-print] [-o dir] [--integer-ids] [--persist-einv] [--inv-store store_file] [filenames ...]',
-                                        usage='python plus2json.pyz play [-v] [-h] [--debug] [--pretty-print] [-o dir] [--integer-ids] [filenames ...]',
-                                        description='Process the PLUS file(s) and output runtime data corresponding to each job definition.',
-                                        epilog=textwrap.dedent('''\
-                                            Examples:
-                                                # load Tutorial_1.puml and print a human readable stream of events to the console
-                                                python plus2json.pyz play Tutorial_1.puml --pretty-print
-
-                                                # load Tutorial_1.puml and print a JSON stream of events to the console
-                                                python plus2json.pyz play Tutorial_1.puml
-
-                                                # load all .puml files in the 'puml' directory and write steam of events for each to a JSON file in 'job_definitions'
-                                                python plus2json.pyz play -o job_definitions puml/*.puml
-                                        '''))
-    play_options = play_parser.add_argument_group(title='Play Options')
+    # play specific options
+    play_options = parser.add_argument_group(title='Play Options')
     play_options.add_argument('--integer-ids', action='store_true', help='Use deterministic integer IDs')
     # play_options.add_argument('--persist-einv', action='store_true', help='Persist external invariants in a file store')
     # play_options.add_argument('--inv-store', metavar='store_file', help='Location to persist external invariant values', default='p2jInvariantStore')
-    play_parser.set_defaults(func=play)
 
+    # parse command line
     args = parser.parse_args()
 
     # configure logging
@@ -95,8 +81,11 @@ def main():
         logging.root.removeHandler(handler)
     logging.basicConfig(stream=sys.stderr, level=(logging.DEBUG if args.debug else logging.INFO), format='%(levelname)s: %(message)s')
 
-    if hasattr(args, 'func'):
-        args.func(**vars(args))
+    # execute the command
+    if args.job:
+        job(**vars(args))
+    elif args.play:
+        play(**vars(args))
     else:
         check(**vars(args))
 
