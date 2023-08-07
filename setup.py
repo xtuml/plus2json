b8db78a57561d1b08cd978bac016f1df1d8d24c1
Levi Starrett <levi@roxsoftware.com>
2023-08-07 10:12:59 -0400

    Quickfix: add kafka as a dependency in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
           keywords='xtuml bridgepoint protocol verifier',
           packages=['plus2json', 'plus2json.plus', 'plus2json.schema'],
           package_data={'plus2json': ['version.json'], 'plus2json.plus': ['*.interp', '*.tokens'], 'plus2json.schema': ['plus_schema.sql']},
-          install_requires=['antlr4-python3-runtime==4.13.0', 'pyxtuml==2.3.1'],
+          install_requires=['antlr4-python3-runtime==4.13.0', 'pyxtuml==2.3.1', 'kafka-python3==3.0.0'],
           test_suite='tests',
           include_package_data=True,
           zip_safe=True)
